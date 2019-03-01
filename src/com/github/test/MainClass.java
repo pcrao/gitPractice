3843d91e23f9b08307bd6559f2b318a19acb11af
PoornaChandraRao Bellam (pcrao) <PoornaChandraRao Bellam poorna.bellam@outlook.com>
2019-03-01 12:54:26 +0530

    Added main class wit hmain method
    This is the entry method to this entire project to run

---
+++ src/com/github/test/MainClass.java
@@ -0,0 +1,10 @@
+package com.github.test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is first print statement");
+	}
+
+}
